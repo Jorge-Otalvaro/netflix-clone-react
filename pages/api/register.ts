d74f37ec7c9d60edf0d2dcb2fe1aa1d433c318af
Jorge Otalvaro <george159302@gmail.com>
2023-04-14 19:43:48 -0500

    Register and Auth success

---
+++ pages/api/register.ts
@@ -0,0 +1,48 @@
+import bcrypt from 'bcrypt';
+import { NextApiRequest, NextApiResponse } from 'next';
+import prismadb from '@/lib/prismadb';
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+    if (req.method !== 'POST') {
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+
+    try {
+        const { name, email, password } = req.body;        
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const userExists = await prismadb.user.findUnique({
+            where: {
+                email,
+            },
+        });
+
+        if (userExists) {
+            return res.status(400).json({ error: 'User already exists' });
+        }          
+
+        const user = await prismadb.user.create({
+            data: {
+                name,
+                email,
+                hashedPassword,
+                image: '',
+                emailVerified: new Date(),
+            },
+        });
+
+        return res.status(200).json(user);
+        
+    } catch (error) {
+
+        console.log(error);
+        res.status(400).json({ 
+            error: error, 
+        });
+
+        return res.status(400).end();
+    }
+}
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,9 +1,13 @@
 import Input from "@/components/Input";
 import { useCallback, useState } from "react";
+import axios from "axios";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const Auth = () => {
+    const router = useRouter();
     
-    const [username, setUsername] = useState("");
+    const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -13,6 +17,44 @@ const Auth = () => {
         setVariant((prev) => (prev === "login" ? "register" : "login"));
     }, []);
 
+    const handleLogin = useCallback(async () => {
+        try
+          {
+             const result = await signIn("credentials", {
+                 redirect: false,
+                 email,
+                 password,
+                 callbackUrl: "/",        
+             });
+
+            router.push("/");         
+          }
+          catch (error) {
+             console.log(error);
+          }
+     }, [email, password, router]);
+
+    const handleSubmit = useCallback(async () => {
+        try {
+            const res = await axios.post("/api/register", {
+                name,
+                email,
+                password,
+            });
+
+            if (res.status === 200) {
+                handleLogin();
+            }            
+        } catch (error) {
+            console.log(error);
+        }
+    }, [
+        name,
+        email,
+        password, 
+        handleLogin
+    ]);
+
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -21,23 +63,25 @@ const Auth = () => {
                 </nav>
                 <div className="flex justify-center">
                     <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+
                         <h2 className="text-white text-4xl mb-8 font-semibold">
-                            {variant === "login" ? "Sign In" : "Register"}
+                            {variant === "login" ? "Inicia sesión" : "Registro"}
                         </h2>
+
                         <div className="flex flex-col gap-4">
                             {variant === "register" && (
                             <Input
                                 id="username"
-                                label="Username"
+                                label="Usuario"
                                 type="text"
-                                value={username}
-                                onChange={(e: any) => setUsername(e.target.value)}
+                                value={name}
+                                onChange={(e: any) => setName(e.target.value)}
                             />
                             )}
 
                             <Input
                                 id="email"
-                                label="Email"
+                                label="Correo electrónico"
                                 type="email"
                                 value={email}
                                 onChange={(e: any) => setEmail(e.target.value)}
@@ -45,19 +89,21 @@ const Auth = () => {
 
                             <Input
                                 id="password"
-                                label="Password"
+                                label="Contraseña"
                                 type="password"
                                 value={password}
                                 onChange={(e: any) => setPassword(e.target.value)}
-                            />                            
+                            />
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-                            {variant === "login" ? "Sign In" : "Register"}
+
+                        <button onClick={variant === "login" ? handleLogin : handleSubmit } className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                            {variant === "login" ? "Iniciar sesión" : "Registrarse"}
                         </button>
+                        
                         <p className="text-neutral-500 mt-12">
-                            {variant === "login" ? "Don't have an account?" : "Already have an account?"}
+                            {variant === "login" ? "¿No tienes una cuenta?" : "¿Ya tienes una cuenta?"}
                             <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
-                                {variant === "login" ? "Create an account" : "Sign In"}
+                                {variant === "login" ? "Crear nueva cuenta" : "Inicia sesión"}
                             </span>
                         </p>
                     </div>
